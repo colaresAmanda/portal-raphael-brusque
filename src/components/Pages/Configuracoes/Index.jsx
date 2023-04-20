adcb45fd45a9a190e14efcbf31cc53dde8436d3b
colaresAmanda <colares.amd@gmail.com>
2023-04-20 19:19:08 -0300

    sidebar is done

--- src/components/Pages/Configuracoes/Index.jsx
+++ src/components/Pages/Configuracoes/Index.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React,{ useState} from 'react'
 
 import './configuracoes.css'
 // Pages
 import Sidebar from './Sidebar/Index.jsx'
 
 function Index() {
+  const SIDEBARDEFAULTOPTIONS  = {
+    "Estrutura da Escola": {
+      Cursos: true,
+      Turmas: false,
+      Disciplinas: false,
+    },
+    "Usuários": {
+      Alunos: false,
+      'Funcionários': false,
+    },
+    "Minhas Informações": {
+      Perfil: false,
+    },
+  }
+  const [sidebarOptionsStatus, setSidebarOptionsStatus] = useState(SIDEBARDEFAULTOPTIONS)
+
+
+
+
   return (
     <section className='section configuracoes-section'>
-        <Sidebar />
-        
+        <Sidebar sidebarOptionsStatus={sidebarOptionsStatus} setSidebarOptionsStatus={setSidebarOptionsStatus} DEFAULTOPTIONS={SIDEBARDEFAULTOPTIONS}/>
+
+
+        <div className="content">
+          <h2>Configurações</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur accusantium cupiditate illo tenetur ratione ducimus consequatur hic officia ipsa itaque reprehenderit reiciendis tempore voluptatem, placeat non, impedit consequuntur, et eius.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, accusantium mollitia nihil temporibus eligendi deserunt! Sed eligendi voluptatem consectetur dicta reprehenderit numquam quas vero asperiores! Tempore quos voluptas sequi iste.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum error ipsum praesentium vero nostrum maiores repudiandae expedita, ullam illo, aut harum est cumque possimus laudantium molestias dignissimos temporibus! Qui, exercitationem!</p>
+        </div>
     </section>
   )
 }
--- src/components/Pages/Configuracoes/Sidebar/Index.jsx
+++ src/components/Pages/Configuracoes/Sidebar/Index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 import { Description } from "../ConfiguracoesText"
 
 // Assets
@@ -6,28 +6,21 @@ import "./sidebar.css"
 import { RiArrowRightSLine } from "react-icons/ri"
 
 // Components
-import SidebarOptions from './SidebarOptions.jsx'
-function Index() {
-  const DEFAULTOPTIONS  = {
-    "Estrutura da Escola": {
-      Cursos: false,
-      Turmas: false,
-      Disciplinas: false,
-    },
-    "Usuários": {
-      Alunos: false,
-      'Funcionários': false,
-    },
-    "Minhas Informações": {
-      Perfil: false,
-    },
-  }
-  
-  const [sidebarOptionsStatus, setSidebarOptionsStatus] = useState(DEFAULTOPTIONS)
+import SidebarOptions from "./SidebarOptions.jsx"
+function Index({
+  sidebarOptionsStatus,
+  setSidebarOptionsStatus,
+  DEFAULTOPTIONS,
+}) {
 
- 
+
+    const [closeSidebar, setCloseSidebar] = useState(true)
+
+  
   function SelectOption(section, index) {
     let arr = JSON.parse(JSON.stringify(DEFAULTOPTIONS))
+
+    arr["Estrutura da Escola"].Cursos = false
 
     if (section === Object.keys(DEFAULTOPTIONS)[0]) {
       if (index === 0) {
@@ -49,23 +42,39 @@ function Index() {
       }
     }
 
-     setSidebarOptionsStatus(arr)
+    setSidebarOptionsStatus(arr)
   }
+
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${closeSidebar ? "hidden" : ""}`}>
       <div className="sidebar-header">
         <h2 className="sidebar-title">Configurações</h2>
-        <button>
+        <button onClick={() => setCloseSidebar(!closeSidebar)}>
           <RiArrowRightSLine />
         </button>
       </div>
       <p className="sidebar-description">{Description.Direcao}</p>
       <div className="sidebar-menu">
-        <SidebarOptions subtitle={"Estrutura da Escola"} options={["Cursos", "Turmas", "Disciplinas"]} sidebarOptionsStatus={sidebarOptionsStatus} selectOption={SelectOption}/>
+        <SidebarOptions
+          subtitle={"Estrutura da Escola"}
+          options={["Cursos", "Turmas", "Disciplinas"]}
+          sidebarOptionsStatus={sidebarOptionsStatus}
+          selectOption={SelectOption}
+        />
 
-        <SidebarOptions subtitle={"Usuários"} options={["Alunos", "Funcionários"]} sidebarOptionsStatus={sidebarOptionsStatus} selectOption={SelectOption}/>
-        
-        <SidebarOptions subtitle={"Minhas Informações"} options={["Perfil"]} sidebarOptionsStatus={sidebarOptionsStatus} selectOption={SelectOption}/>
+        <SidebarOptions
+          subtitle={"Usuários"}
+          options={["Alunos", "Funcionários"]}
+          sidebarOptionsStatus={sidebarOptionsStatus}
+          selectOption={SelectOption}
+        />
+
+        <SidebarOptions
+          subtitle={"Minhas Informações"}
+          options={["Perfil"]}
+          sidebarOptionsStatus={sidebarOptionsStatus}
+          selectOption={SelectOption}
+        />
       </div>
     </div>
   )
