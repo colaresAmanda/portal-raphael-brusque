13a1ad17503b237d84fb4f066c00b061abb6a792
colaresAmanda <colares.amd@gmail.com>
2023-04-20 12:14:55 -0300

    sidebar options working

--- src/components/Pages/Configuracoes/Sidebar/Index.jsx
+++ src/components/Pages/Configuracoes/Sidebar/Index.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { Description } from "../ConfiguracoesText"
 
 // Assets
@@ -8,7 +8,7 @@ import { RiArrowRightSLine } from "react-icons/ri"
 // Components
 import SidebarOptions from './SidebarOptions.jsx'
 function Index() {
-  const defaultOptions = {
+  const DEFAULTOPTIONS  = {
     "Estrutura da Escola": {
       Cursos: false,
       Turmas: false,
@@ -16,14 +16,41 @@ function Index() {
     },
     "Usuários": {
       Alunos: false,
-      Funcionarios: false,
+      'Funcionários': false,
     },
     "Minhas Informações": {
       Perfil: false,
     },
   }
+  
+  const [sidebarOptionsStatus, setSidebarOptionsStatus] = useState(DEFAULTOPTIONS)
 
+ 
+  function SelectOption(section, index) {
+    let arr = JSON.parse(JSON.stringify(DEFAULTOPTIONS))
 
+    if (section === Object.keys(DEFAULTOPTIONS)[0]) {
+      if (index === 0) {
+        arr["Estrutura da Escola"].Cursos = true
+      } else if (index === 1) {
+        arr["Estrutura da Escola"].Turmas = true
+      } else if (index === 2) {
+        arr["Estrutura da Escola"].Disciplinas = true
+      }
+    } else if (section === Object.keys(DEFAULTOPTIONS)[1]) {
+      if (index === 0) {
+        arr["Usuários"].Alunos = true
+      } else if (index === 1) {
+        arr["Usuários"]["Funcionários"] = true
+      }
+    } else if (section === Object.keys(DEFAULTOPTIONS)[2]) {
+      if (index === 0) {
+        arr["Minhas Informações"].Perfil = true
+      }
+    }
+
+     setSidebarOptionsStatus(arr)
+  }
   return (
     <div className="sidebar">
       <div className="sidebar-header">
@@ -34,11 +61,11 @@ function Index() {
       </div>
       <p className="sidebar-description">{Description.Direcao}</p>
       <div className="sidebar-menu">
-        <SidebarOptions subtitle={"Estrutura da Escola"} options={["Cursos", "Turmas", "Disciplinas"]} defaultOptions={defaultOptions}/>
+        <SidebarOptions subtitle={"Estrutura da Escola"} options={["Cursos", "Turmas", "Disciplinas"]} sidebarOptionsStatus={sidebarOptionsStatus} selectOption={SelectOption}/>
 
-        <SidebarOptions subtitle={"Usuários"} options={["Alunos", "Funcionários"]} defaultOptions={defaultOptions}/>
+        <SidebarOptions subtitle={"Usuários"} options={["Alunos", "Funcionários"]} sidebarOptionsStatus={sidebarOptionsStatus} selectOption={SelectOption}/>
         
-        <SidebarOptions subtitle={"Minhas Informações"} options={["Perfil"]} defaultOptions={defaultOptions}/>
+        <SidebarOptions subtitle={"Minhas Informações"} options={["Perfil"]} sidebarOptionsStatus={sidebarOptionsStatus} selectOption={SelectOption}/>
       </div>
     </div>
   )
--- src/components/Pages/Configuracoes/Sidebar/SidebarOptions.jsx
+++ src/components/Pages/Configuracoes/Sidebar/SidebarOptions.jsx
@@ -1,45 +1,19 @@
-import  { useState } from "react"
-
-function SidebarOptions({ subtitle, options, defaultOptions}) {
-
-  const [sidebarOptions, setSidebarOptions] = useState(defaultOptions)
-
-  function SelectOption(section, index) {
-    let arr = defaultOptions
-
-    if (section === Object.keys(defaultOptions)[0]) {
-      if (index === 0) {
-        arr["Estrutura da Escola"].Cursos = true
-      } else if (index === 1) {
-        arr["Estrutura da Escola"].Turmas = true
-      } else if (index === 2) {
-        arr["Estrutura da Escola"].Disciplinas = true
-      }
-
-
-    } else if (section === Object.keys(defaultOptions)[1]) {
-      if (index === 0) {
-        arr["Usuários"].Alunos = true
-      } else if (index === 1) {
-        arr["Usuários"].Funcionarios = true
-      }
-
-
-    } else if (section === Object.keys(defaultOptions)[2]) {
-      if (index === 0) {
-        arr["Minhas Informações"].Perfil = true
-      }
-    }
-    setSidebarOptions(arr)
-  }
-
-  console.log(sidebarOptions)
+function SidebarOptions({
+  subtitle,
+  options,
+  sidebarOptionsStatus,
+  selectOption,
+}) {
   return (
     <>
       <h3 className="sidebar-subtitle">{subtitle}</h3>
       <div className="menu-options">
         {options.map((option, index) => (
-          <button key={index} onClick={() => SelectOption(subtitle, index)} >
+          <button
+            className={sidebarOptionsStatus[`${subtitle}`][`${option}`] ? "selected" : ""}
+            key={index}
+            onClick={() => selectOption(subtitle, index)}
+          >
             {option}
           </button>
         ))}
